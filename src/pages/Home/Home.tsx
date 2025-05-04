43a87e77c520ead1c6608cc173347c1a12f0cd81
remolacho <jonathan.rojas@rankmi.com>
2025-05-03 22:33:45 -0500

    manejo de carpetas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { ChatBox } from "../components/@Analytics";
 import './Home.scss';
 
 export const Home: React.FC = () => {
   return (
     <div className="home">
-      <ChatBox />
     </div>
   );
 }; 
